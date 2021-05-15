fc52949504b4d1f0554c1128c8d1eb78a8737a67
UCDT <adrian.j.ulibarri@gmail.com>
2021-05-15 12:51:44 -0600

    basic outline established

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,106 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+        //author section
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your projects name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }  
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the link to your deployed appication?',
+            validate: appLink => {
+                if (appLink) {
+                    return true;
+                } else {
+                    console.log('Please provide a link to your deployed application!')
+                    return false;
+                }
+            }
+        },
+        // table of contents section
+        {
+            type: 'confirm',
+            name: 'confirmTable',
+            message: 'Would you like to add and item to the "Table of contents" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'table',
+            message: 'Provide the name of a section to add to the table of contents',
+            when: ({ confirmTable }) => {
+                if (confirmTable) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        // install instructions section
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Please provide any special install instructions for your application!'
+        },
+        
+        // usage section
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions on how to use your application! (required)',
+            validate: usage => {
+                if (usage) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        
+        //credits section
+        {
+            // colaborator and link to profile on github
+            //asset, creator, and link to primary web presence
+            // tutorials used, link
+        },
+        // license(s) section
+        {
+            //give a list of license's to chose from
+        },
+        //badges section
+        {
+            
+        },
+        // features section
+        {
+
+        },
+        //Contributing section
+        {
+
+        },
+        // tests section
+        {
+
+        }
+    ])
+}
+
 const questions = [];
 
 // TODO: Create a function to write README file
